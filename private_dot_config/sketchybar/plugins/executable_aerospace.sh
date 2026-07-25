1be5e1d020340954da4da1846d08962808acccf6
Aleksandr Emelianov <jemelyah@gmail.com>
2026-07-25 23:08:45 +0200

    Style sketchybar: bar background/blur/rounded corners, default fonts/colors, active-workspace pill, divider

--- private_dot_config/sketchybar/plugins/executable_aerospace.sh
+++ private_dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -3,7 +3,7 @@ SID="${NAME#space.}"
 FOCUSED="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
 
 if [ "$SID" = "$FOCUSED" ]; then
-  sketchybar --set "$NAME" background.drawing=on background.color=0xff44475a label.color=0xffffffff
+  sketchybar --set "$NAME" background.drawing=on background.color=0xff89b4fa label.color=0xff1e1e2e
 else
-  sketchybar --set "$NAME" background.drawing=off label.color=0xffaaaaaa
+  sketchybar --set "$NAME" background.drawing=off label.color=0xff9399b2
 fi
