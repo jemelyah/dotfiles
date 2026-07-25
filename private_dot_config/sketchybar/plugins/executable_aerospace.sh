5f78bbd0a591fe23806f4ed94e95fe0b569a0f1e
Aleksandr Emelianov <jemelyah@gmail.com>
2026-07-25 23:21:53 +0200

    Drop sketchybar, use native macOS menu bar instead. Remove FelixKratz/formulae tap and exec-on-workspace-change hook.

--- private_dot_config/sketchybar/plugins/executable_aerospace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SID="${NAME#space.}"
-FOCUSED="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
-
-if [ "$SID" = "$FOCUSED" ]; then
-  sketchybar --set "$NAME" background.drawing=on background.color=0xff89b4fa label.color=0xff1e1e2e
-else
-  sketchybar --set "$NAME" background.drawing=off label.color=0xff9399b2
-fi
