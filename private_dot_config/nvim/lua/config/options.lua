83261c5c643848816b4b82055600ea293bfbd6f0
Aleksandr Emelianov <jemelyah@gmail.com>
2026-08-01 01:03:30 +0200

    nvim: add Spanish and Latvian to spelllang

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.spelllang = { "en", "ru" }
+vim.opt.spelllang = { "en", "ru", "es", "lv" }
 
 vim.g.lazyvim_ruby_lsp = "ruby_lsp"
 vim.g.lazyvim_ruby_formatter = "rubocop"
